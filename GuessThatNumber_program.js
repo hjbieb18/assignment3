fca2bc9a6e3271e2920d6ab10b20f4201a94abed
hjbieb18 <54552567+hjbieb18@users.noreply.github.com>
2019-10-08 16:00:02 -0500

    Jquery

--- GuessThatNumber_program.js
+++ GuessThatNumber_program.js
@@ -5,41 +5,49 @@
 
 
 
-    (function ($.newGame) {
-        $('#window').click(function() {
-            location.reload();
-    }
+    $(function(newGame){
+      window.location.reload();
+    })
+        
 
-    function init() {
+   
+    
+
+    $(function(init) {
 
         compGuess = Math.floor(Math.random() * 100 + 1);
         // console.log(compGuess);
-    }
+    })
+
+
            
- 
-    function compareGuess() {
+// document.getElementById(“myElement”) to $(‘#myElement’)
 
-        var userGuess =" " + document.getElementById("inputBox").value;
-        // console.log(userGuess);
 
-        userGuessLog.push(userGuess);
-        // console.log(userGuessLog);
-        document.getElementById("guessLog").innerHTML = userGuessLog;
-        score++;
-        document.getElementById("score").innerHTML = score;
+    $(function(compareGuess) {
 
+        var userGuess =" " + $('#inputBox'){
+            // console.log(userGuess);
+            userGuessLog.push(userGuess);
+            // console.log(userGuessLog);
+            document.getElementById("guessLog").innerHTML = userGuessLog;
+            score++;
+            document.getElementById("score").innerHTML = score;
+        }
         if(userGuess > compGuess) {
-            document.getElementById("textOutput").innerHTML = 'Too High';
-            document.getElementById('inputBox').value = "";
+                document.getElementById("textOutput").innerHTML = 'Too High';
+                document.getElementById('inputBox').value = "";
         }
         else if (userGuess < compGuess) {
-            document.getElementById("textOutput").innerHTML = 'Too Low';
-            document.getElementById("inputBox").value = "";
+                document.getElementById("textOutput").innerHTML = 'Too Low';
+                document.getElementById("inputBox").value = "";
         }
         else {
             document.getElementById("textOutput").innerHTML = 'You Are Correct';
         }
-    }
+    })
+            
+        
     function buttonSpinner() {
         
         guessButton.onclick = () =>{
